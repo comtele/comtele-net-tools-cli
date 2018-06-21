066a186618f3a1539a0a0f89a7225db88c0367ad
Diego Vieira <divieira.silva@gmail.com>
2018-06-21 09:48:33 -0300

    Add project files.

---
+++ Comtele.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comtele.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
